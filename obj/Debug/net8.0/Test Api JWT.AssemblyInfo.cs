9617c9ae1ee38774fed40074a591b9640176cc21
Abraam Adel Demitry <126262425+Abraam318@users.noreply.github.com>
2024-07-23 09:40:04 +0300

    Models Creating

--- obj/Debug/net8.0/Test Api JWT.AssemblyInfo.cs
+++ obj/Debug/net8.0/Test Api JWT.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test Api JWT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c74acebc96ee31cc99cede07b4c028d7a9b8cb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test Api JWT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test Api JWT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
